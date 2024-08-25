6502f37cefa36cd757ffb3dfe7b41e93127fcdd6
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2024-08-25 13:11:31 +0859

    chore: not use hono in static build

--- src/main.tsx
+++ src/main.tsx
@@ -1,41 +1,18 @@
 /* @jsx h */
 
-import type { InferResponseType } from "hono/client";
-import { hc } from "hono/client";
-import { useEffect, useState } from "hono/jsx";
-import { ApiType } from "~/functions/api/root.ts";
 import { h, render } from "preact";
 
-const root = document.getElementById("mount");
+const mount = document.getElementById("mount");
 
-if (root) {
-  render(<App />, root!);
+if (mount) {
+  render(<App />, mount!);
 }
 
 function App() {
-  const client = hc<ApiType>("/");
-  const $get = client.api.hello.$get;
-
-  const [data, setData] = useState<InferResponseType<typeof $get>>();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await $get({
-        query: {
-          name: "Pages",
-        },
-      });
-      const responseData = await res.json();
-      setData(responseData);
-    };
-    fetchData();
-  }, []);
-
   return (
     <main>
       <div>
         <h1>hello</h1>
-        <h1>{data?.message}</h1>
       </div>
     </main>
   );
